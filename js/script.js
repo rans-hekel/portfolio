8d631e0e571caede24e90db24a1c0745dcd1a0e5
rans-hekel <ransenju27@gmail.com>
2021-02-24 16:18:34 +0700

    menambahkan paralax dan memperbaiki navbar

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ window.onscroll = function(){
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
 }
+
+// untuk sidenav
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav);
+
+// untuk parallax
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax)
+
+// untuk navbar
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()>710){
+            $('nav').addClass('green');
+        }else{
+            $('nav').removeClass('green');
+        }
+    });
+});
