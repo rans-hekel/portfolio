c4107376784bb9ddf08f6448dfa295de147d38b9
rans-hekel <ransenju27@gmail.com>
2021-03-05 23:38:48 +0700

    menambahkan landing element 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+// untuk opening
+const gambar = document.querySelector('.container img');
+const headerangga = document.querySelector('.container h3');
+const headerangga2 = document.querySelector('.container p');
+
+$(window).on("load",function(){
+    $(gambar).css({
+        "transform" : "scale(1)",
+        "opacity" : 1
+    })
+    setTimeout(function() {
+
+        $(headerangga).css({
+            "transform" : "translate(0,0)",
+            "opacity" : 1
+        })
+        $(headerangga2).css({
+            "transform" : "translate(0,0)",
+            "opacity" : 1
+        })
+
+
+    },  1500)
+})
+
+// untuk loading
 let progress = document.getElementById('progressbar');
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function(){
@@ -17,9 +43,16 @@ M.Parallax.init(parallax)
 $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop()>710){
-            $('nav').addClass('green');
+            $('nav').addClass('grey');
         }else{
-            $('nav').removeClass('green');
+            $('nav').removeClass('grey');
         }
     });
 });
+
+// untuk navbar2
+window.addEventListener('scroll', function(){
+    let header = document.querySelector('nav');
+    header.classList.toggle('sticky', window.scrollY > 710);
+});
+
